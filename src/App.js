987c620717a492f9f955d84fe501dbf5afefcb2c
robinhood8865 <robinhood8865@gmail.com>
2022-08-01 22:41:57 +0900

    part2-5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-function App(props) {
+function App() {
   
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note')
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(()=>{
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/notes')
+    .then(response=>{
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  },[])
+  console.log('render', notes.length, 'notes')
 
   const notesToShow = showAll ? notes : notes.filter(note=>note.important === true)
   
@@ -19,11 +31,16 @@ function App(props) {
       important: Math.random()<0.5,
       id: notes.length +1,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(noteObject))
+      setNewNote('')
+    })
+    
   }
   const handleNoteChange = (event) =>{
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewNote(event.target.value)
   }
   
